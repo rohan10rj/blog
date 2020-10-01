7e65d7dac09fc72bd2056da0b0104b0807062f8b
Rohan Golawar <67593768+rohan10rj@users.noreply.github.com>
2020-10-01 23:55:22 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+---Django's command-line utility for administrative tasks for betterment in user access.---
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codersarena.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CodersArena.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
